12fdf94034db3ae003878737a732a0d085d6ec16
devontehillman <hillmand@mail.gvsu.edu>
2024-05-13 23:21:44 -0400

    adding defaults to shorten if else statements

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-	const argv = require("yargs/yargs")(process.argv.slice(2))
-		//.usage('Usage: -lat [num] -lon [num]')
-		.option("latitude", {
-			alias: "lat",
-			description: "Latitude coordinate",
-			type: "number",
-			demandOption: false, // Required option
-		})
-		.option("longitude", {
-			alias: "lon",
-			description: "Longitude coordinate",
-			type: "number",
-			demandOption: false, // Required option
-		})
-		.option("hourly", {
-			alias: "h",
-			description: "Request for hourly forecast",
-			type: "boolean",
-			demandOption: true, // Required option
-		}).argv;
+const argv = require("yargs/yargs")(process.argv.slice(2))
+	//.usage('Usage: -lat [num] -lon [num]')
+	.option("latitude", {
+		alias: "lat",
+		default: 39.7456,
+		description: "Latitude coordinate",
+		type: "number",
+		demandOption: false, // Required option
+	})
 
-	console.log("(%d,%d.%s)", argv.latitude, argv.longitude, argv.hourly);
+	.option("longitude", {
+		alias: "lon",
+		default: -97.0892,
+		description: "longitude coordinate",
+		type: "number",
+		demandOption: false, // Required option
+	})
+	.option("hourly", {
+		alias: "h",
+		default: false,
+		description: "Request for hourly forecast",
+		type: "boolean",
+		demandOption: false, // Required option
+	});
+
+console.log("(%d,%d.%s)", argv.latitude, argv.longitude, argv.hourly);
 
 if (argv.latitude === undefined || argv.longitude === undefined) {
 	const url = "https://api.weather.gov/points/39.7456,-97.0892";
