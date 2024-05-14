f566cdf8fcd863dc240dfd1bd08fb96eb9b5d107
devontehillman <hillmand@mail.gvsu.edu>
2024-05-14 11:48:28 -0400

    converting double to single quotes, adding boxen for hourly, error handling

--- index.js
+++ index.js
@@ -3,27 +3,26 @@ import boxen from 'boxen';
 import yargs from 'yargs/yargs';
 
 const argv = yargs(process.argv.slice(2))
-	//.usage('Usage: -lat [num] -lon [num]')
-	.option("latitude", {
-		alias: "lat",
+	.option('latitude', {
+		alias: 'lat',
 		default: 39.7456,
-		description: "Latitude coordinate",
-		type: "number",
+		description: 'Latitude coordinate',
+		type: 'number',
 		demandOption: false, // Required option
 	})
 
-	.option("longitude", {
-		alias: "lon",
+	.option('longitude', {
+		alias: 'lon',
 		default: -97.0892,
-		description: "longitude coordinate",
-		type: "number",
+		description: 'longitude coordinate',
+		type: 'number',
 		demandOption: false, // Required option
 	})
-	.option("hourly", {
-		alias: "h",
+	.option('hourly', {
+		alias: 'h',
 		default: false,
-		description: "Request for hourly forecast",
-		type: "boolean",
+		description: 'Request for hourly forecast',
+		type: 'boolean',
 		demandOption: false, // Required option
 	})
 	.argv;
@@ -31,7 +30,7 @@ const argv = yargs(process.argv.slice(2))
 console.log(`Latitude: ${argv.latitude}`);
 console.log(`Longitude: ${argv.longitude}`);
 
-console.log("(%d,%d.%s)", argv.latitude, argv.longitude, argv.hourly);
+console.log('(%d,%d.%s)', argv.latitude, argv.longitude, argv.hourly);
 
 if (argv.hourly) {
 	const url = `https://api.weather.gov/points/${argv.latitude},${argv.longitude}`;
@@ -42,15 +41,15 @@ if (argv.hourly) {
 }
 
 function getWeatherHourly(url) {
-	//Url for api call
+	// Url for api call
 
 	const request = fetch(url)
-		//convert json text to json object
+		// convert json text to json object
 		.then((response) => response.json())
-		//getting the endpoint url form json object that contains the forecast
+		// getting the endpoint url form json object that contains the forecast
 		.then((data) => {
 			const endpoint = data.properties.forecastHourly;
-			//console.log(endpoint)
+			// console.log(endpoint)
 			return fetch(endpoint);
 		})
 		// converting json text to json object
@@ -63,23 +62,32 @@ function getWeatherHourly(url) {
 			// Obtaining only the first 12
 			const firstTwelvePeriods = WeeklyForecastByHour.slice(0, 12);
 
+			let forecast = '';
 			firstTwelvePeriods.forEach((period) => {
 				// print first 12
-				console.log(`${period.temperature} with ${period.shortForecast} \n`);
+				forecast += `${period.temperature} with ${period.shortForecast} \n`
 			});
+
+			const boxenOptions = { title: 'Weather App', padding: 1, margin: 1, borderStyle: 'double' };
+			const message = 'Hourly Forecast\n\n' + forecast;
+
+			console.log(boxen(message, boxenOptions));
+		})
+		.catch((error) => {
+			console.error('Error:', error);
 		});
 }
 
 function getWeather(url) {
-	//Url for api call
+	// Url for api call
 
 	const request = fetch(url)
-		//convert json text to json object
+		// convert json text to json object
 		.then((response) => response.json())
-		//getting the endpoint url form json object that contains the forecast
+		// getting the endpoint url form json object that contains the forecast
 		.then((data) => {
 			const endpoint = data.properties.forecast;
-			console.log(endpoint)
+			console.log(endpoint);
 			return fetch(endpoint);
 		})
 		// converting json text to json object
@@ -90,17 +98,18 @@ function getWeather(url) {
 
 			const WeeklyForecast = data.properties.periods;
 
-			//console.log(WeeklyForecast[0])
+			// console.log(WeeklyForecast[0])
 			const temp = String(WeeklyForecast[0].temperature);
-			const humidity = String(WeeklyForecast[0].relativeHumidity['value'])
-			const windSpeed = WeeklyForecast[0].windSpeed
-			const forecast = WeeklyForecast[0].detailedForecast
-
+			const humidity = String(WeeklyForecast[0].relativeHumidity['value']);
+			const windSpeed = WeeklyForecast[0].windSpeed;
+			const forecast = WeeklyForecast[0].detailedForecast;
 
 			const boxenOptions = { title: 'Weather App', padding: 1, margin: 1, borderStyle: 'double' };
-			const message = `Temperature: ${temp}\nHumidity: ${humidity}\nWind Speed:${windSpeed}\n\n${forecast}`
+			const message = 'Weekly Forecast\n\n' +`Temperature: ${temp}\nHumidity: ${humidity}\nWind Speed:${windSpeed}\n\n${forecast}`;
 
 			console.log(boxen(message, boxenOptions));
-
+		})
+		.catch((error) => {
+			console.error('Error:', error);
 		});
 }
